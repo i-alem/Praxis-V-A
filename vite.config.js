41eea7cf73c4f9e5886aff0cac6694dc4489a9e4
Alem Imamovic <56873154+i-alem@users.noreply.github.com>
2025-08-11 17:15:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    copyPublicDir: false,
-    assetsInlineLimit: 0,
     rollupOptions: {
       input: {
         main: 'index.html',
@@ -15,7 +12,6 @@ export default defineConfig({
       }
     }
   },
-  assetsInclude: ['**/*.svg'],
   server: {
     port: 3000,
     open: true
