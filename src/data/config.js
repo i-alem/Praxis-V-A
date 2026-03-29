5dcfe12f805ec0917011d7f81f30af9a88d7c2b0
Alem Imamovic <56873154+i-alem@users.noreply.github.com>
2026-03-29 19:56:53 +0200

    style update

--- src/data/config.js
+++ src/data/config.js
@@ -4,7 +4,6 @@ export const PRAXIS = {
     street:     'Kaiserdamm 9',
     city:       '14059 Berlin',
     phone:      '+49 179 12345678',
-    phoneTel:   '+49 179 12345678',
     email:      'info@praxis-v-a.com',
     bookingUrl: 'https://medifox.de/termine',
     mapsUrl:    'https://www.google.com/maps/search/Kaiserdamm+9+14059+Berlin',
@@ -33,7 +32,7 @@ export function contactBlock() {
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>
             </svg>
             <div class="contact-text">
-                <a href="tel:${PRAXIS.phoneTel}">${PRAXIS.phone}</a>
+                <a href="tel:${PRAXIS.phone.replace(/\s/g, '')}">${PRAXIS.phone}</a>
             </div>
         </div>
         <div class="contact-item">
